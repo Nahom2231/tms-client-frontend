79aa4ed6cc3c5ab8e8504a25af42f893c2b018cc
Nahom2231 <nahom0462@gmail.com>
2026-07-31 17:15:53 +0300

    fix: removing a stray character in app.html and updating the button in course-detail.html to navigate through Angular router.

--- src/app/features/course-detail/course-detail.ts
+++ src/app/features/course-detail/course-detail.ts
@@ -3,6 +3,7 @@ import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-course-detail',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './course-detail.html',
   styleUrl: './course-detail.scss',
